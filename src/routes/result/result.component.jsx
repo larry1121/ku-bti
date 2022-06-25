4f8c16680154e501223babbdb562357e9badb6f8
Yoomin Kang <mintway0341@gmail.com>
2022-06-25 19:36:16 +0900

    Design: "add button-start scss design"

--- src/routes/result/result.component.jsx
+++ src/routes/result/result.component.jsx
@@ -1,15 +1,22 @@
 import Mbtis from "../../common/api/mbtisApi.json";
 import { useParams } from "react-router-dom";
 
-const Result = ({ match }) => {
+const Result = () => {
   const { mbtiName } = useParams();
   console.log(mbtiName);
   console.log(Mbtis);
   const mbti = Mbtis[mbtiName];
   console.log(mbti);
+  if (!mbti) {
+    return (
+      <div>
+        <h1>This is an invalid page.</h1>
+      </div>
+    );
+  }
   return (
     <div>
-      <h1>This is a result page.</h1>
+      <h1>This is an {mbtiName} page.</h1>
     </div>
   );
 };
